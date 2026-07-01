4a468100c85cfdc80b8c1b0615f46b4d9a3aab4f
Chris Collins <kuroneko-git@sysadninjas.net>
2026-07-01 17:24:20 +1000

    CMake: only build/install certain things on Linux or unixlikes.
    * Don't bother trying to build the servers under Windows.
    
    * Don't install the freedesktop file under anything other than Linux.
    
    * dont' install snis_launcher except under unixlikes.

--- cmake/install_snis_data.cmake
+++ cmake/install_snis_data.cmake
@@ -2,18 +2,19 @@
 # This cmake script is executed during install in order to deploy the SNIS assets and to build the desktop icon
 #
 
-make_directory("$ENV{DESTDIR}${SNIS_DATA_DIR}")
+file(MAKE_DIRECTORY "$ENV{DESTDIR}${SNIS_DATA_DIR}")
 execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/snis_update_assets --force --destdir $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX} --srcdir ${CMAKE_CURRENT_SOURCE_DIR}/../share/snis)
 
-set(DESKTOP_PATH "${CMAKE_INSTALL_PREFIX}/share/applications")
-
-make_directory($ENV{DESTDIR}${DESKTOP_PATH})
-configure_file(../share/applications/io.github.smcameron.space-nerds-in-space.desktop.tmpl 
-    "$ENV{DESTDIR}${DESKTOP_PATH}/io.github.smcameron.space-nerds-in-space.desktop"
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
-    @ONLY)
-file(INSTALL
-    ../share/applications/io.github.smcameron.space-nerds-in-space.svg
-    DESTINATION $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/applications
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ)
-execute_process(COMMAND update-desktop-database $ENV{DESTDIR}${DESKTOP_PATH})
+if(CMAKE_SYSTEM_NAME MATCHES Linux)
+    set(DESKTOP_PATH "${CMAKE_INSTALL_PREFIX}/share/applications")
+    file(MAKE_DIRECTORY $ENV{DESTDIR}${DESKTOP_PATH})
+    configure_file(../share/applications/io.github.smcameron.space-nerds-in-space.desktop.tmpl
+        "$ENV{DESTDIR}${DESKTOP_PATH}/io.github.smcameron.space-nerds-in-space.desktop"
+        FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
+        @ONLY)
+    file(INSTALL
+        ../share/applications/io.github.smcameron.space-nerds-in-space.svg
+        DESTINATION $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/applications
+        FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ)
+    execute_process(COMMAND update-desktop-database $ENV{DESTDIR}${DESKTOP_PATH})
+endif()
